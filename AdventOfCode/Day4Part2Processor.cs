63e0dddbd2ffa6340609da2aa9138229d7b499e1
Victoria Usan <victoria.usan@gmail.com>
2025-12-06 18:53:45 +0100

    Day 4 Part 2

---
+++ AdventOfCode/Day4Part2Processor.cs
@@ -0,0 +1,95 @@
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+namespace adventofcode;
+
+public class Day4Part2Processor : IDayProcessor
+{
+    public void ProcessFile(string dayPath, string selectedFile)
+    {
+        Console.WriteLine($"Processing Day 4 specific file: {selectedFile}");
+
+        try
+        {
+            using StreamReader sr = new(Path.Combine(dayPath, selectedFile));
+            string? line;
+
+            List<List<char>> matrix = [];
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    matrix.Add([.. line.ToCharArray()]);
+                }
+            }
+
+            if (matrix.Count == 0 || matrix[0].Count == 0)
+            {
+                Console.WriteLine("Matrix is empty. No elements to process.");
+                return;
+            }
+
+            int rows = matrix.Count;
+            int cols = matrix[0].Count;
+            int totalCountOfSymbols = 0;
+            bool changed;
+
+            do
+            {
+                changed = false;
+                List<(int r, int c)> toChange = [];
+
+                for (int r = 0; r < rows; r++)
+                {
+                    for (int c = 0; c < cols; c++)
+                    {
+                        if (matrix[r][c] == '@')
+                        {
+                            int adjacentAtCount = 0;
+                            for (int dr = -1; dr <= 1; dr++)
+                            {
+                                for (int dc = -1; dc <= 1; dc++)
+                                {
+                                    if (dr == 0 && dc == 0) continue;
+
+                                    int newR = r + dr;
+                                    int newC = c + dc;
+
+                                    if (newR >= 0 && newR < rows && newC >= 0 && newC < cols && matrix[newR][newC] == '@')
+                                    {
+                                        adjacentAtCount++;
+                                    }
+                                }
+                            }
+
+                            if (adjacentAtCount < 4)
+                            {
+                                toChange.Add((r, c));
+                            }
+                        }
+                    }
+                }
+
+                if (toChange.Count > 0)
+                {
+                    changed = true;
+                    totalCountOfSymbols += toChange.Count;
+                    foreach (var (r, c) in toChange)
+                    {
+                        matrix[r][c] = '.';
+                    }
+                }
+
+            } while (changed);
+
+            Console.WriteLine($"Total count of elements with less than 4 adjacent \'@\': {totalCountOfSymbols}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read for Day 4 Part 2:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
